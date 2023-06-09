2f2ba15061fcba750ca84b914bbd4255a0cad07d
aiuhjv1 <43039187+ajisrael@users.noreply.github.com>
2023-06-08 21:46:57 -0400

    f   - added FindAddressQuery

--- src/main/java/control/tower/address/service/query/AddressesQueryHandler.java
+++ src/main/java/control/tower/address/service/query/AddressesQueryHandler.java
@@ -2,11 +2,15 @@ package control.tower.address.service.query;
 
 import control.tower.address.service.core.data.AddressEntity;
 import control.tower.address.service.core.data.AddressRepository;
+import control.tower.address.service.query.queries.FindAddressQuery;
+import control.tower.address.service.query.queries.FindAllAddressesQuery;
 import lombok.AllArgsConstructor;
 import org.axonframework.queryhandling.QueryHandler;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+
+import static control.tower.core.utils.Helper.throwErrorIfEntityDoesNotExist;
 
 @Component
 @AllArgsConstructor
@@ -17,5 +21,15 @@ public class AddressesQueryHandler {
     @QueryHandler
     public List<AddressEntity> findAllAddresses(FindAllAddressesQuery query) {
         return addressRepository.findAll();
+    }
+
+    @QueryHandler
+    public AddressEntity findAddress(FindAddressQuery query) {
+        AddressEntity addressEntity = addressRepository.findByAddressId(query.getAddressId());
+
+        throwErrorIfEntityDoesNotExist(addressEntity,
+                String.format("Address %s does not exist", query.getAddressId()));
+
+        return addressEntity;
     }
 }
