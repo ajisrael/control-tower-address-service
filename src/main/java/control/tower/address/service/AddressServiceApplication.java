798a024f06c5eaad7392b3a2ad6f9d3e64df674f
ajisrael <43039187+ajisrael@users.noreply.github.com>
2023-06-23 13:32:31 -0400

    b   - added xstream config for forbidden class exception

--- src/main/java/control/tower/address/service/AddressServiceApplication.java
+++ src/main/java/control/tower/address/service/AddressServiceApplication.java
@@ -3,6 +3,7 @@ package control.tower.address.service;
 import control.tower.address.service.command.interceptors.CreateAddressCommandInterceptor;
 import control.tower.address.service.command.interceptors.RemoveAddressCommandInterceptor;
 import control.tower.address.service.core.errorhandling.AddressServiceEventsErrorHandler;
+import control.tower.core.config.XStreamConfig;
 import org.axonframework.commandhandling.CommandBus;
 import org.axonframework.config.EventProcessingConfigurer;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +11,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Import;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@Import({ XStreamConfig.class })
 public class AddressServiceApplication {
 
 	public static void main(String[] args) {
